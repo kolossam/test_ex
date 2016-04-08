23be6f72e8bb10695e00194e76dc641151baaecf
eav <kolossam@yandex.ru>
2016-04-08 18:11:23 +0300

    first commit

--- 3.sql
+++ 3.sql
@@ -1,3 +1,21 @@
+
+-- Table structure
+CREATE TABLE `data` (
+  `id` SERIAL,
+  `type` VARCHAR(6) NOT NULL,
+  `date` DATE NOT NULL,
+  `value` INTEGER UNSIGNED NOT NULL
+);
+
+-- Table sample data
+INSERT INTO `data` VALUES
+  (null, 'photo', '2015-02-02', 1240),
+  (null, 'image', '2015-02-02', 5609),
+  (null, 'photo', '2015-02-01', 1190),
+  (null, 'review', '2015-02-02', 3600);
+
+
+
 --- postgresql
 
 WITH tmp as (SELECT "type", max("date") "date"
